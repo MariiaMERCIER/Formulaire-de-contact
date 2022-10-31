a1f8e9e4a9ffe929c430f58e4c1206c72e939bfc
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-10-31 08:05:22 +0100

    #

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,11 @@ document
 .addEventListener("submit", async (event)=>{
     event.preventDefault();
     const data = {
-        nom: document.querySelector("nom").value,
-        prenom: document.querySelector("prenom").value,
-        email: document.querySelector("email").value,
-        sujet: document.querySelector("sujet").value,
-        message: document.querySelector("message").value
+        nom: document.querySelector("#nom").value,
+        prenom: document.querySelector("#prenom").value,
+        email: document.querySelector("#email").value,
+        sujet: document.querySelector("#sujet").value,
+        message: document.querySelector("#message").value
     };
 
     console.log(data);
